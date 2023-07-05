dde8a1a2e2e6e0a010574fe9c4a6211ac1ae38c8
yinonozery <yinonozery@gmail.com>
2023-07-05 14:39:13 +0300

    Set up angular project and basic functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
+
 export class AppComponent {
+  constructor(private snackBar: MatSnackBar) { }
+
   title = 'PasswordGenerator';
+  useLettersLower: boolean = false;
+  useLettersUpper: boolean = false;
+  useNumbers: boolean = false;
+  useSymbols: boolean = false;
+  buttonDisabled: boolean = false;
+  length: number = 0;
+  password: string | null = null;
+
+  generatePassword = () => {
+    const numbers = '1234567890';
+    const letters_lower = 'abcdefghijklmnopqrstuvwxyz';
+    const letters_upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const symbols = '!@#$%^&*()';
+    let validChars: string = '', password: string = '';
+
+    validChars += this.useLettersLower ? letters_lower : '';
+    validChars += this.useLettersUpper ? letters_upper : '';
+    validChars += this.useNumbers ? numbers : '';
+    validChars += this.useSymbols ? symbols : '';
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      password += validChars[index];
+    }
+    return password;
+  }
+
+  onButtonClick = (): void => {
+    console.log(`Generating a password with the following:
+    Length: ${this.length}
+    Uppercase Letters: ${this.useLettersUpper}
+    Lowercase Letters: ${this.useLettersLower}
+    Numbers: ${this.useNumbers}
+    Symbols: ${this.useSymbols}`)
+    this.password = this.generatePassword();
+  }
+
+  onChangeLength = (target: EventTarget): void => {
+    const value = (<HTMLInputElement>target).value;
+    const parsedValue = parseInt(value);
+    this.length = parsedValue;
+  }
+
+  onChangeUseLettersUpper = (): void => {
+    this.useLettersUpper = !this.useLettersUpper;
+  }
+
+  onChangeUseLettersLower = (): void => {
+    this.useLettersLower = !this.useLettersLower;
+  }
+
+  onChangeUseNumbers = (): void => {
+    this.useNumbers = !this.useNumbers;
+  }
+
+  onChangeUseSymbols = (): void => {
+    this.useSymbols = !this.useSymbols;
+  }
+
+  copyToClipboard = (): void => {
+    navigator.clipboard.writeText(this.password || '');
+    this.snackBar.open('Password copied to clipboard', 'OK', {
+      duration: 2000,
+    });
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatInputModule } from '@angular/material/input';
 import { AppComponent } from './app.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSliderModule,
+    MatSnackBarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
